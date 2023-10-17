7dd9311e59b1de55725aba4e00740e5e1f50beda
Séverin Lemaignan <severin.lemaignan@pal-robotics.com>
2023-10-17 14:20:36 +0200

    port ari_bringup to ROS2

---
+++ ari_bringup/launch/ari_bringup.launch.py
@@ -0,0 +1,50 @@
+# Copyright (c) 2023 PAL Robotics S.L. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from launch import LaunchDescription
+from launch_pal.include_utils import include_launch_py_description
+
+
+def generate_launch_description():
+    default_controllers = include_launch_py_description(
+        "ari_controller_configuration", [
+            "launch", "default_controllers.launch.py"]
+    )
+
+    play_motion2 = include_launch_py_description(
+        "ari_bringup", ["launch", "ari_play_motion2.launch.py"]
+    )
+
+    twist_mux = include_launch_py_description(
+        "ari_bringup", ["launch", "twist_mux.launch.py"]
+    )
+
+    ari_state_publisher = include_launch_py_description(
+        "ari_description", ["launch", "robot_state_publisher.launch.py"]
+    )
+
+    # TODO: robot pose publisher
+    # TODO: tf lookup
+    # TODO: QR code detector
+    # TODO: torso front camera
+    # TODO: torso back camera
+
+    ld = LaunchDescription()
+
+    ld.add_action(default_controllers)
+    ld.add_action(play_motion2)
+    ld.add_action(twist_mux)
+    ld.add_action(ari_state_publisher)
+
+    return ld
---
+++ ari_bringup/launch/ari_play_motion2.launch.py
@@ -0,0 +1,67 @@
+# Copyright (c) 2023 PAL Robotics S.L. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import OpaqueFunction
+
+from launch_pal.arg_utils import read_launch_argument
+from launch_pal.include_utils import include_launch_py_description
+from launch_pal.robot_utils import get_robot_model, get_robot_name
+
+from ari_description.ari_launch_utils import get_ari_hw_suffix
+
+
+def declare_args(context, *args, **kwargs):
+
+    robot_name = read_launch_argument("robot_name", context)
+
+    return [get_robot_model(robot_name)]
+
+
+def launch_setup(context, *args, **kwargs):
+
+    robot_model = read_launch_argument("robot_model", context)
+
+    motions_file = f"ari{get_ari_hw_suffix(robot_model=robot_model)}_motions.yaml"
+
+    play_motion2_config = os.path.join(
+        get_package_share_directory("ari_bringup"), "config", "motions", motions_file
+    )
+
+    play_motion2 = include_launch_py_description(
+        "play_motion2",
+        ["launch", "play_motion2.launch.py"],
+        launch_arguments={"play_motion2_config": play_motion2_config}.items(),
+    )
+
+    return [play_motion2]
+
+
+def generate_launch_description():
+
+    ld = LaunchDescription()
+
+    # Declare arguments
+    # we use OpaqueFunction so the callbacks have access to the context
+    ld.add_action(get_robot_name("ari"))
+    ld.add_action(OpaqueFunction(function=declare_args))
+
+    # Launch play_motion2 with the proper config
+    ld.add_action(OpaqueFunction(function=launch_setup))
+
+    return ld
