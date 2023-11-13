17a4eaa6be89b675cac14e34979a1ebc02020b02
Séverin Lemaignan <severin.lemaignan@pal-robotics.com>
2023-11-13 09:23:44 +0100

    use_sim_time=False by default

--- ari_description/launch/robot_state_publisher.launch.py
+++ ari_description/launch/robot_state_publisher.launch.py
@@ -19,6 +19,7 @@ from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, OpaqueFunction
+from launch.substitutions import LaunchConfiguration
 
 from launch_pal.arg_utils import read_launch_argument
 from launch_pal.robot_utils import (
@@ -35,7 +36,7 @@ from launch_ros.actions import Node
 def declare_args(context, *args, **kwargs):
 
     sim_time_arg = DeclareLaunchArgument(
-        "use_sim_time", default_value="true", description="Use simulation time"
+        "use_sim_time", default_value="false", description="Use simulation time"
     )
 
     robot_name = read_launch_argument("robot_name", context)
@@ -74,7 +75,8 @@ def launch_setup(context, *args, **kwargs):
         package="robot_state_publisher",
         executable="robot_state_publisher",
         output="both",
-        parameters=[robot_description],
+        parameters=[{'use_sim_time': LaunchConfiguration('use_sim_time')},
+                    robot_description],
     )
 
     return [rsp]
