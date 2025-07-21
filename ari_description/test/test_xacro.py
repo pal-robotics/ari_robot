3679313b47a479f9ec82226add56536d3a113720
Susanna <susanna.mastromauro@pal-robotics.com>
2025-07-21 15:29:07 +0200

    Changing test & test_xacro

--- ari_description/test/test_xacro.py
+++ ari_description/test/test_xacro.py
@@ -31,8 +31,6 @@ ee_laser = (
 )
 
 test_xacro_robot_model = define_xacro_test(xacro_file_path, ee_laser)
-test_xacro_laser_model = define_xacro_test(xacro_file_path, AriArgs.robot_model)
-test_xacro_end_effector = define_xacro_test(xacro_file_path, AriArgs.robot_model)
-test_xacro_head_camera_model = define_xacro_test(xacro_file_path, AriArgs.laser_model)
-test_xacro_torso_front_cam = define_xacro_test(xacro_file_path, AriArgs.laser_model)
-test_xacro_torso_back_cam = define_xacro_test(xacro_file_path, AriArgs.laser_model)
+test_xacro_laser_model = define_xacro_test(xacro_file_path, AriArgs.robot_model, AriArgs.end_effector)
+test_xacro_end_effector = define_xacro_test(xacro_file_path, AriArgs.head_camera_model, AriArgs.robot_model)
+test_xacro_head_camera_model = define_xacro_test(xacro_file_path, AriArgs.laser_model, AriArgs.end_effector)
